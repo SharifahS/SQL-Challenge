ead6a479188a429c28eadcaff3747e1a3aaa824c
SharifahS <sharifah.s.sinclair@gmail.com>
2020-11-29 21:52:23 -0500

    commit

--- EmployeeSQL/Table_schemata.sql
+++ EmployeeSQL/Table_schemata.sql
@@ -1,39 +1,39 @@
 CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "emp_title" int   NOT NULL,
+    "emp_no" INT  NOT NULL,
+    "emp_title" VARCHAR(20)   NOT NULL,
     "birth_date" TEXT   NOT NULL,
-    "first_name" varchar(50)   NOT NULL,
-    "last_name" varchar(50)   NOT NULL,
-    "sex" varchar(2)   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "sex" VARCHAR(2)   NOT NULL,
     "hire_date" TEXT   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY ( "emp_no" )
 );
 
 CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL
+    "emp_no" INT  NOT NULL,
+    "dept_no" VARCHAR(20)   NOT NULL
 );
 
 CREATE TABLE "departments" (
-    "dept_no" int   NOT NULL,
-    "dept_name" varchar(50)   NOT NULL
+    "dept_no" VARCHAR(20)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" int   NOT NULL,
-    "emp_no" int   NOT NULL,
+    "dept_no" VARCHAR(20)   NOT NULL,
+    "emp_no" INT   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY ("emp_no" )
 );
 
 CREATE TABLE "titles" (
-    "title_id" int   NOT NULL,
-    "title" varchar(50)   NOT NULL,
+    "title_id" VARCHAR(20)   NOT NULL,
+    "title" VARCHAR(50)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY ("title_id")
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY ( "emp_no" )
 );
 
